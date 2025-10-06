8fc48c8f27f889d95f7cb31e0899c8c69f6ce0f6
unknown <elyas.larfi@ucdenver.edu>
2025-10-05 19:28:57 -0600

    added to init

--- bioneuralnet/network_embedding/__init__.py
+++ bioneuralnet/network_embedding/__init__.py
@@ -1,4 +1,4 @@
 from .gnn_embedding import GNNEmbedding
-from .gnn_models import GCN, GAT, SAGE, GIN
+from .gnn_models import GCN, GAT, SAGE, GIN, Transformer
 
-__all__ = ["GNNEmbedding", "GCN", "GAT", "SAGE", "GIN"]
+__all__ = ["GNNEmbedding", "GCN", "GAT", "SAGE", "GIN", "Transformer"]
